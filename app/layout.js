d07555265eaef0846e1d61017e391dbd12210070
HiraSiddiquiii <140696034+HiraSiddiquiii@users.noreply.github.com>
2026-09-05 08:12:16 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
       </head>
